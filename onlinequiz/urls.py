5c9b8a913a37d1a265608d3198a515a747469cbf
mbp13 <shaxobiddina20@gmail.com>
2023-11-14 11:18:21 +0500

    edit student templates

--- onlinequiz/urls.py
+++ onlinequiz/urls.py
@@ -28,7 +28,7 @@ schema_view = get_schema_view(
 )
 urlpatterns = [
 
-    path('admin/', admin.site.urls),
+    path('admin-super_admin/', admin.site.urls),
     path('swagger<format>.json|.yaml', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     path('doc/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
