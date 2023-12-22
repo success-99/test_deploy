21a1796a3b9f3bc9e6b609a87d35f6bbe203fe67
mbp13 <shaxobiddina20@gmail.com>
2023-12-23 01:05:10 +0500

    edit settings.py and collectstatic

--- onlinequiz/settings.py
+++ onlinequiz/settings.py
@@ -19,7 +19,7 @@ SECRET_KEY = env.str('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG')
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["35.243.132.53"]
 
 # Application definition
 
