b3cbef86aef4b5656a5ca5ee68af884df149fdc6
mbp13 <shaxobiddina20@gmail.com>
2024-01-10 01:51:54 +0500

    railway deploy

--- onlinequiz/settings.py
+++ onlinequiz/settings.py
@@ -18,7 +18,7 @@ SECRET_KEY = env.str('SECRET_KEY')
 
 DEBUG = env.bool('DEBUG')
 
-ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app']
+ALLOWED_HOSTS = ["*"]
 # Application definition
 
 INSTALLED_APPS = [
