f38259fca8082699880040f2659f408bee1f99a3
mbp13 <shaxobiddina20@gmail.com>
2024-01-09 18:43:39 +0500

    varcel bash

--- build.sh
+++ build.sh
@@ -2,11 +2,11 @@
 
 # Build the project
 echo "Building the project..."
-python3.8 -m pip install -r requirements.txt
+python3 -m pip install -r requirements.txt
 
 echo "Make Migration..."
-python3.8 manage.py makemigrations --noinput
-python3.8 manage.py migrate --noinput
+python3 manage.py makemigrations --noinput
+python3 manage.py migrate --noinput
 
 echo "Collect Static..."
-python3.8 manage.py collectstatic --noinput --clear
+python3 manage.py collectstatic --noinput --clear
